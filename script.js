3fb4837cf10d904f861f3f27cb93d820dcd129a4
brajagopaldas63 <brajagopaldas245@gmail.com>
2025-05-01 22:23:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Game(){
+    window.location.href= "game.html";
+}
+function Event(){
+    window.location.href= "event.html";
+}
+function map(){
+    window.location.href= "https://maps.app.goo.gl/49xudUQoCqQA5JCU9";
+}
